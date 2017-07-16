5980da3bc771d619a725b37d02782a2f9057e1e5
Ian Livingstone <ianl@cs.dal.ca>
2017-07-15 22:23:25 -0300

    Add 'Status' column to list command
    Closes manifoldco/engineering#2528

--- cmd/list.go
+++ cmd/list.go
@@ -16,10 +16,13 @@ import (
 	"github.com/manifoldco/manifold-cli/data/catalog"
 	"github.com/manifoldco/manifold-cli/session"
 
-	marketplaceModels "github.com/manifoldco/manifold-cli/generated/marketplace/models"
+	"github.com/manifoldco/manifold-cli/generated/marketplace/client/resource"
+	"github.com/manifoldco/manifold-cli/generated/marketplace/models"
+	"github.com/manifoldco/manifold-cli/generated/provisioning/client/operation"
+	pModels "github.com/manifoldco/manifold-cli/generated/provisioning/models"
 )
 
-type resourcesSortByName []*marketplaceModels.Resource
+type resourcesSortByName []*models.Resource
 
 func (r resourcesSortByName) Len() int {
 	return len(r)
@@ -70,10 +73,21 @@ func list(cliCtx *cli.Context) error {
 		return errNotLoggedIn
 	}
 
-	// Init catalog client
 	catalogClient, err := clients.NewCatalog(cfg)
 	if err != nil {
-		return cli.NewExitError("Failed to create the Catalog API client: "+
+		return cli.NewExitError("Failed to create a Catalog API client: "+
+			err.Error(), -1)
+	}
+
+	marketplaceClient, err := clients.NewMarketplace(cfg)
+	if err != nil {
+		return cli.NewExitError("Failed to create a Marketplace API client: "+
+			err.Error(), -1)
+	}
+
+	pClient, err := clients.NewProvisioning(cfg)
+	if err != nil {
+		return cli.NewExitError("Failed to create a Provisioning API Client: "+
 			err.Error(), -1)
 	}
 
@@ -83,32 +97,32 @@ func list(cliCtx *cli.Context) error {
 		return cli.NewExitError("Failed to fetch catalog data: "+err.Error(), -1)
 	}
 
-	// Init marketplace client
-	marketplaceClient, err := clients.NewMarketplace(cfg)
-	if err != nil {
-		return cli.NewExitError("Failed to create the Marketplace API client: "+
-			err.Error(), -1)
-	}
-
 	// Get resources
-	res, err := marketplaceClient.Resource.GetResources(nil, nil)
+	res, err := marketplaceClient.Resource.GetResources(
+		resource.NewGetResourcesParamsWithContext(ctx), nil)
 	if err != nil {
 		return cli.NewExitError("Failed to fetch the list of provisioned "+
 			"resources: "+err.Error(), -1)
 	}
 
+	// Get operations
+	oRes, err := pClient.Operation.GetOperations(
+		operation.NewGetOperationsParamsWithContext(ctx), nil)
+	if err != nil {
+		return cli.NewExitError("Failed to fetch the list of operations: "+err.Error(), -1)
+	}
+
+	resources, statuses := buildResourceList(res.Payload, oRes.Payload)
+
 	// Sort resources by name and filter by given app name
-	resources := filterResourcesByAppName(res.Payload, appName)
+	resources = filterResourcesByAppName(resources, appName)
 	sort.Sort(resourcesSortByName(resources))
 
 	// Write out the resources table
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 8, ' ', 0)
-	fmt.Fprintln(w, "Resource Name\tApp Name\tProduct\tPlan\tRegion")
+	fmt.Fprintln(w, "Resource Name\tApp Name\tStatus\tProduct\tPlan\tRegion")
 	for _, resource := range resources {
 		appName := string(resource.Body.AppName)
-		if appName == "" {
-			appName = "None"
-		}
 
 		// Get catalog data
 		product, err := catalog.GetProduct(resource.Body.ProductID.String())
@@ -127,9 +141,79 @@ func list(cliCtx *cli.Context) error {
 				err.Error(), -1)
 		}
 
-		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n", resource.Body.Name,
-			appName, product.Body.Name, plan.Body.Name, region.Body.Name)
+		status, ok := statuses[resource.ID]
+		if !ok {
+			status = "Ready"
+		}
+
+		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\n", resource.Body.Name,
+			appName, status, product.Body.Name, plan.Body.Name, region.Body.Name)
 	}
 	w.Flush()
 	return nil
+}
+
+func buildResourceList(resources []*models.Resource, operations []*pModels.Operation) (
+	[]*models.Resource, map[manifold.ID]string) {
+	out := []*models.Resource{}
+	statuses := make(map[manifold.ID]string)
+
+	for _, op := range operations {
+		switch op.Body.Type() {
+		case "provision":
+			body := op.Body.(*pModels.Provision)
+			if body.State == nil {
+				panic("State value was nil")
+			}
+
+			// if its a terminal state, then we can just ignore the op
+			if *body.State == "done" || *body.State == "error" {
+				continue
+			}
+
+			statuses[op.Body.ResourceID()] = "Creating"
+			out = append(out, &models.Resource{
+				ID: op.Body.ResourceID(),
+				Body: &models.ResourceBody{
+					AppName:   manifold.Name(body.AppName),
+					CreatedAt: op.Body.CreatedAt(),
+					UpdatedAt: op.Body.UpdatedAt(),
+					Label:     manifold.Label(*body.Label),
+					Name:      manifold.Name(*body.Name),
+					PlanID:    body.PlanID,
+					ProductID: body.ProductID,
+					RegionID:  body.RegionID,
+					UserID:    op.Body.UserID(),
+				},
+			})
+		case "resize":
+			body := op.Body.(*pModels.Resize)
+			if body.State == nil {
+				panic("State value was nil")
+			}
+
+			if *body.State == "done" || *body.State == "error" {
+				continue
+			}
+
+			statuses[op.Body.ResourceID()] = "Resizing"
+		case "deprovision":
+			body := op.Body.(*pModels.Deprovision)
+			if body.State == nil {
+				panic("State value was nil")
+			}
+
+			if *body.State == "done" || *body.State == "error" {
+				continue
+			}
+
+			statuses[op.Body.ResourceID()] = "Deleting"
+		}
+	}
+
+	for _, r := range resources {
+		out = append(out, r)
+	}
+
+	return out, statuses
 }
