bef5a9d5f7d18d87e4ba4e1432f58685b321e97a
҈҈҈Luiz Branco <me@luizbranco.com>
2017-08-30 16:48:34 -0400

    Add teams list command (#70)

--- clients/teams.go
+++ clients/teams.go
@@ -9,6 +9,12 @@ import (
 	"github.com/manifoldco/manifold-cli/prompts"
 )
 
+// TeamMembersCount groups a team name with the amount of members the team has
+type TeamMembersCount struct {
+	Name    string
+	Members int
+}
+
 // FetchTeams returns the teams for the authenticated user
 func FetchTeams(ctx context.Context, c *iClient.Identity, shouldSpin bool) ([]*iModels.Team, error) {
 	if shouldSpin {
@@ -21,13 +27,73 @@ func FetchTeams(ctx context.Context, c *iClient.Identity, shouldSpin bool) ([]*i
 	if err != nil {
 		return nil, err
 	}
+	return res.Payload, nil
+}
 
-	var results []*iModels.Team
-	for _, t := range res.Payload {
-		results = append(results, t)
+// FetchTeamMembers returns a list of members profile from a team.
+func FetchTeamMembers(ctx context.Context, id string, c *iClient.Identity, shouldSpin bool) ([]*iModels.MemberProfile, error) {
+	if shouldSpin {
+		spin := prompts.NewSpinner("Fetching memberships")
+		spin.Start()
+		defer spin.Stop()
+	}
+	params := team.NewGetTeamsIDMembersParamsWithContext(ctx)
+	params.SetID(id)
+	res, err := c.Team.GetTeamsIDMembers(params, nil)
+	if err != nil {
+		return nil, err
+	}
+	return res.Payload, nil
+}
+
+// FetchTeamsMembersCount returns a list of all user teams with their names and
+// number of members.
+func FetchTeamsMembersCount(ctx context.Context, c *iClient.Identity, shouldSpin bool) ([]TeamMembersCount, error) {
+	if shouldSpin {
+		spin := prompts.NewSpinner("Fetching memberships")
+		spin.Start()
+		defer spin.Stop()
+	}
+	teams, err := FetchTeams(ctx, c, false)
+	if err != nil {
+		return nil, err
 	}
 
-	return results, nil
+	// team payload doesn't contain a list of members. In order to find the
+	// number for each team we fetch them in parallel.
+	res := make(chan TeamMembersCount, len(teams))
+	fail := make(chan error)
+
+	for _, t := range teams {
+		id := t.ID.String()
+		name := string(t.Body.Name)
+
+		go func() {
+			members, err := FetchTeamMembers(ctx, id, c, false)
+
+			if err != nil {
+				fail <- err
+				return
+			}
+
+			res <- TeamMembersCount{
+				Name:    name,
+				Members: len(members),
+			}
+		}()
+	}
+
+	var counts []TeamMembersCount
+	for range teams {
+		select {
+		case err := <-fail:
+			return nil, err
+		case count := <-res:
+			counts = append(counts, count)
+		}
+	}
+
+	return counts, nil
 }
 
 // FetchMemberships returns all memberships for the authenticated user
