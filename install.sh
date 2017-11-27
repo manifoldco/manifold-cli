bb97fde4b2af80cff8e3916e34caad5b967a367b
Luiz Branco <me@luizbranco.com>
2017-11-27 14:42:15 -0500

    Release 0.11.0

--- install.sh
+++ install.sh
@@ -118,7 +118,7 @@
   REPO="https://github.com/manifoldco/manifold-cli"
 
   if [ -z "$MANIFOLD_VERSION" ]; then
-    MANIFOLD_VERSION="0.10.1"
+    MANIFOLD_VERSION="0.11.0"
   fi
 
   if [ -z "$MANIFOLD_DIR" ]; then
